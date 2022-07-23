cfdbabdc1e3603ffd52fc64dd8be8ac9b760b569
uriel batista <uri.batista@hotmail.com>
2022-07-22 23:57:38 -0300

    Modify token authentication expired authorization

--- src/services/getAuthToken.tsx
+++ src/services/getAuthToken.tsx
@@ -4,6 +4,7 @@ import envs from '../Config/env';;
 export async function GetAuthenticationToken() {
     const { CLIENT_ID, CLIENT_SECRET, GRANT_TYPE, API_AUTH } = envs;
     const url = `${API_AUTH}client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=${GRANT_TYPE}`;
+    console.log(url)
     const response = await axios.post(url);
     return response.data;
 }
